d4624c556fa6ec8ffc756d90b4b3741484451943
Simorgh <104884978+arshavant@users.noreply.github.com>
2025-09-03 17:03:55 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
-IMAGE_DIR = Path("images")
-MODEL_DIR = Path("models")
+IMAGE_PATH = Path("images")
+MODEL_PATH = Path("models")
+CASCADE_PATH = Path("haarcascade_frontalface_default.xml")
 
 FACE_SIZE = (200, 200)
 CONFIDENCE_THRESHOLD = 80
-
-CASCADE_PATH = str("haarcascade_frontalface_default.xml")
+NUM_CAPTURE_IMAGES = 300
